bfdc62060ea27f65f5e899f549fbf276326bb15d
JulyJun <bag3401@gmail.com>
2023-11-07 17:09:02 +0900

    flash still working

--- STM_CLK/Debug/Core/Src/main.d
+++ STM_CLK/Debug/Core/Src/main.d
@@ -35,7 +35,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
- ../Core/Inc/lcd.h ../Core/Inc/main.h
+ ../Core/Inc/lcd.h ../Core/Inc/main.h ../Core/Inc/flash.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -75,3 +75,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
 ../Core/Inc/lcd.h:
 ../Core/Inc/main.h:
+../Core/Inc/flash.h:
